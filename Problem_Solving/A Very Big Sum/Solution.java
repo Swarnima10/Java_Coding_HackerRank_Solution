d79469c87ba85f0e4f008b0eb155ed98ed93a324
Swarnima10 <75361619+Swarnima10@users.noreply.github.com>
2021-01-11 10:58:16 +0530

    Create Solution.java

---
+++ Problem_Solving/A Very Big Sum/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		long sum = 0;
+
+		for (int i = 0; i < n; i++) {
+			sum += scan.nextInt();
+		}
+		scan.close();
+		System.out.println(sum);
+
+	}
+
+}
