02b33283d8f268bd4bc07d57b828a46cf1d3c1fa
Swarnima10 <75361619+Swarnima10@users.noreply.github.com>
2021-05-04 11:43:58 +0530

    Create Solution.java

---
+++ Problem_Solving/Beautiful Days At The Movies/Solution.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int count = 0;
+        int start_no = scan.nextInt();
+        int end_no = scan.nextInt();
+        int divisor = scan.nextInt();
+        for (int i = start_no; i <= end_no; i++) {
+            int number = i, reverse = 0;
+            while (number != 0) {
+                int remainder = number % 10;
+                reverse = reverse * 10 + remainder;
+                number = number / 10;
+            }
+            if (Math.abs(i - reverse) % divisor == 0) {
+                count++;
+            }
+
+        }
+        System.out.print(count);
+    }
+
+}
