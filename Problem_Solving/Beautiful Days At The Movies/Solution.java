8890bfa0436bcfd3e3e4ccf6c1012b4fb2e78bbc
Swarnima10 <75361619+Swarnima10@users.noreply.github.com>
2021-05-04 11:49:12 +0530

    Update Solution.java

--- Problem_Solving/Beautiful Days At The Movies/Solution.java
+++ Problem_Solving/Beautiful Days At The Movies/Solution.java
@@ -33,3 +33,32 @@ public class Solution {
     }
 
 }
+
+//Solution 2
+//efficient way of reversing the number
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		Scanner scan = new Scanner(System.in);
+		int count = 0;
+		int start_no = scan.nextInt();
+		int end_no = scan.nextInt();
+		int divisor = scan.nextInt();
+		for (int i = start_no; i <= end_no; i++) {
+			
+			if (Math.abs(i - reverse(i)) % divisor == 0) {
+				count++;
+			}
+
+		}
+		System.out.print(count);
+	}
+
+	private static int reverse(int i) {
+		StringBuilder sb=new StringBuilder();
+		sb.append(i);
+		sb.reverse();
+		return Integer.parseInt(sb.toString());
+	}
+
+}
+
